d2da6c3d2f7ea6d6db05ecd3e5c948081d80e36c
Utpanna Pradhan <jina@Utpannas-MacBook-Air.local>
2026-05-11 13:06:21 +0530

    added structure

---
+++ src/constants/theme.ts
@@ -0,0 +1,17 @@
+export const COLORS = {
+    background:"#F8FAFC",
+    card:"#FFFFFF",
+    borders:"#E2E8F0",
+
+    textPrimary:"#0F172A",
+    textSecondary:"#64748B",
+
+    primary:"#4F46E5",
+    primaryHover:"#4338CA",
+
+    sucess:"#10B981",
+    warning:"#F59E0B",
+    danger:"#EF4444",
+
+    sidebar:"#FFFFFF",
+}
